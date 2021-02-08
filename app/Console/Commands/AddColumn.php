2bb784c0cbfdc16da06a4d5cd5220a04cf1b90bc
khatn <kha.tn@neo-lab.vn>
2021-02-08 13:36:08 +0700

    [LA-2] Update command column:add (null)

--- app/Console/Commands/AddColumn.php
+++ app/Console/Commands/AddColumn.php
@@ -14,6 +14,7 @@ class AddColumn extends Command
      */
     protected $signature = 'column:add
             {--t | type= : Type of column}
+            {--i | isNull= : Null or not null(Choose y or n)}
             {column_name    : Name of column}
             {table_name : Name of table}';
 
@@ -45,12 +46,64 @@ class AddColumn extends Command
         $objectOptions = $this->options();
         $table_name = $objectArguments['table_name'];
         $column_name = $objectArguments['column_name'];
-        $type = $objectOptions['type'];
-        if(Schema::hasTable($table_name)){
-            $this->info("Table existed!");
+        $type = $objectOptions['type'] != null ? $objectOptions['type'] : 'text';
+        if($objectOptions['isNull']=='y'){
+            $isNull = '->nullable()';
+        }
+        else if($objectOptions['isNull']=='n'){
+            $isNull = '';
         }
         else{
-            $this->error("Table doesn't exist!");
+            $this->error('Only choose y or n');
+        }
+
+        if(Schema::hasTable($table_name)){
+            if(Schema::hasColumn($table_name,$column_name)){
+                $this->info("Column '$column_name' in table '$table_name' existed!");
+            }
+            else{
+                //$this->error("Column '$column_name' in table '$table_name' doesn't exist!");
+                $path = base_path()."/database/migrations/".date('Y_m_d_His')."_add_column_$column_name"."_for_table_$table_name.php";
+                $myfile = fopen($path,"w");
+                $txt = "<?php
+
+                    use Illuminate\Database\Migrations\Migration;
+                    use Illuminate\Database\Schema\Blueprint;
+                    use Illuminate\Support\Facades\Schema;
+
+                    class AddColumn".ucfirst($column_name)."ForTable".ucfirst($table_name)." extends Migration
+                    {
+                        /**
+                         * Run the migrations.
+                         *
+                         * @return void
+                         */
+                        public function up()
+                        {
+                            Schema::table('$table_name', function (Blueprint \$table) {
+                                //
+                                \$table->$type('$column_name')$isNull;
+                            });
+                        }
+
+                        /**
+                         * Reverse the migrations.
+                         *
+                         * @return void
+                         */
+                        public function down()
+                        {
+                            Schema::table('users', function (Blueprint \$table) {
+                                //
+                            });
+                        }
+                    }";
+                fwrite($myfile,$txt);
+                fclose($myfile);
+            }
+        }
+        else{
+            $this->error("Table '$table_name' existed!");
         }
     }
 }
